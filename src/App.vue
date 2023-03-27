e491d8b59ad88c60c85029596851425be1edc46e
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2023-03-27 10:58:15 -0300

    :tada: Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue';
+
+</script>
+
+<template>
+    <TodoApp />
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0%;
+  }
+  #app {
+    font-family: Avenir, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialised;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+</style>
